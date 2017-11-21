dbdc97244cdd9cf3966430d1fff97d737f396654
Chadyred <chadyred@gmail.com>
2017-11-21 16:47:51 +0100

    Wip bahat
    End behat

---
+++ tests/functionals/features/bootstrap/Contexts/Video.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Functionals\Contexts;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Tests\Functionals\FeatureContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class Video extends FeatureContext
+{
+}
