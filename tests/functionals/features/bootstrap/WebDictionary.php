c97d9a6a37cbbe6dce4b37d32f061513fa7777d1
Chadyred <chadyred@gmail.com>
2017-11-22 15:37:14 +0100

    Add click

--- tests/functionals/features/bootstrap/WebDictionary.php
+++ tests/functionals/features/bootstrap/WebDictionary.php
@@ -7,4 +7,13 @@ namespace Tests\Functionals;
 */
 trait WebDictionary
 {
+    /**
+     * @When I click on :arg1
+     */
+    public function clickCss($selector)
+    {
+        $link = $this->find('css', $selector);
+        $link->click();
+        return $this;
+    }
 }
