cd624d6b93b620a34f1a34f113e2e0fb94d9253c
Chadyred <chadyred@gmail.com>
2017-11-23 11:27:09 +0100

    Add --dev

--- .rocketeer/tasks.php
+++ .rocketeer/tasks.php
@@ -1,7 +1,7 @@
 <?php
 	use Rocketeer\Facades\Rocketeer;
-	
-	Rocketeer::task('composer', 'composer install');
-	Rocketeer::task('php bin/console', 'd:d:c --if-not-exists');
-	Rocketeer::task('php bin/console', 'd:s:u');
-	Rocketeer::task('php bin/console', 'cache:clear e=prod');
+
+	Rocketeer::task('composer', 'composer install --dev');
+	Rocketeer::task('create database', 'php bin/console d:d:c --if-not-exists');
+	Rocketeer::task('update database', 'php bin/console d:s:u');
+	Rocketeer::task('clear cache', 'php bin/console cache:clear e=prod');
